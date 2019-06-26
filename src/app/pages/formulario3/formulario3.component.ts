009de0d621bc3a07957f6ebf8924c94c9826b730
Caio Barbosa <37249463+Caio002Barbosa@users.noreply.github.com>
2019-06-26 10:32:37 -0300

    Exclusão de bibliotecas desnecessárias

--- src/app/pages/formulario3/formulario3.component.ts
+++ src/app/pages/formulario3/formulario3.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Alert } from '../../model/alert.model';
 import { CompararCampos } from './validacaoCustomizada';
 
