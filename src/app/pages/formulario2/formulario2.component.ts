3e0f09c28920df0dade64a67e8b58cbb3356ffd8
Caio Barbosa <37249463+Caio002Barbosa@users.noreply.github.com>
2019-06-26 10:31:57 -0300

    Correção de bibliotecas

--- src/app/pages/formulario2/formulario2.component.ts
+++ src/app/pages/formulario2/formulario2.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Alert } from '../../model/alert.model';
-
 import { validarData } from './validacaoCustomizada';
 
 @Component({
