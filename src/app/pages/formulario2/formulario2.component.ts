7b573877c6e1cccb8ea7a5778f95603d057229e2
Caio Barbosa <37249463+Caio002Barbosa@users.noreply.github.com>
2019-06-26 10:28:47 -0300

    Exclusão de bibliotecas desnecessárias

--- src/app/pages/formulario2/formulario2.component.ts
+++ src/app/pages/formulario2/formulario2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { Alert } from '../../model/alert.model';
 
